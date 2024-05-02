47529a1df5be95e925e6c8ea14f3e39da544834e
SleimaD <sleima@icloud.com>
2024-05-02 15:29:20 +0200

    add of project model

--- portfolio/models.py
+++ portfolio/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
-# Create your models here.
+class Project(models.Model):
+    title = models.CharField(max_length=200)  
+    category = models.CharField(max_length=100)  
+    image_description = models.TextField(blank=True) 
+    image = models.ImageField(upload_to='images/') 
