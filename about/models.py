b4ce3bc42d74daf5b3b3e1e1285cacc1e8826ef3
SleimaD <sleima@icloud.com>
2024-05-02 15:02:55 +0200

    add of About model

--- about/models.py
+++ about/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+class About(models.Model):
+    title = models.CharField(max_length=200)
+    subtitle = models.CharField(max_length=200, blank=True)
+    profile_image = models.ImageField(upload_to='images/')
+    general_description = models.TextField()
+    birthday = models.DateField()
+    website = models.URLField(blank=True)
+    phone = models.CharField(max_length=20)
+    city = models.CharField(max_length=100)
+    age = models.IntegerField()
+    degree = models.CharField(max_length=100)
+    email = models.EmailField()
+    freelance = models.CharField(max_length=100)
+    detailed_description = models.TextField()    
