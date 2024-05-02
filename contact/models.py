d67341c72524760a967f1f073ae8fbba80802569
SleimaD <sleima@icloud.com>
2024-05-02 16:09:40 +0200

    add of contact model

--- contact/models.py
+++ contact/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+class ContactInfo(models.Model):
+    description = models.TextField()
+    street = models.CharField(max_length=200)
+    street_number = models.CharField(max_length=20)
+    location = models.CharField(max_length=255)
+    zipcode = models.CharField(max_length=100)
+    email = models.EmailField(max_length=100)
+    phone = models.CharField(max_length=100)
